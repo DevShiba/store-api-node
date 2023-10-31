bfe06c974c8515b9c24d17eae489d53164347ee8
DevShiba <migerushiba@gmail.com>
2023-10-31 16:10:26 -0300

    Finishing the Project

--- app.js
+++ app.js
@@ -1,43 +1,41 @@
-require('dotenv').config()
-require('express-async-errors')
+require('dotenv').config();
+require('express-async-errors');
 
 const express = require('express');
 const app = express();
 
-const connectDB = require('./db/connect')
-const productsRouter = require('./routes/products')
+const connectDB = require('./db/connect');
+const productsRouter = require('./routes/products');
 
-const notFoundMiddleware = require('./middleware/not-found')
-const errorMiddleware = require('./middleware/error-handler')
+const notFoundMiddleware = require('./middleware/not-found');
+const errorMiddleware = require('./middleware/error-handler');
 
-//async errors
-
+// middleware
 app.use(express.json());
 
-//routes
+// routes
 
-app.get('/', (req, res)=>{
-    res.send('<h1>Store API</h1><a href="api/v1/products/">products route</a>')
-})
+app.get('/', (req, res) => {
+  res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>');
+});
 
-app.use('/api/v1/products',productsRouter)
+app.use('/api/v1/products', productsRouter);
 
-//product route
+// products route
 
-app.use(notFoundMiddleware)
-app.use(errorMiddleware)
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
+const start = async () => {
+  try {
+    // connectDB
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => console.log(`Server is listening port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-const start = async()=>{
-    try {
-        //conectDB
-        await connectDB(process.env.MONGO_URI)
-        app.listen(port, console.log(`Server is listening port ${port}`))
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-start()
+start();
--- routes/products.js
+++ routes/products.js
@@ -1,7 +1,10 @@
-const express = require('express');
+const express = require('express')
 const router = express.Router()
 
-const {getAllProductsStatic, getAllProducts} = require('../controllers/products')
+const {
+  getAllProducts,
+  getAllProductsStatic,
+} = require('../controllers/products')
 
 router.route('/').get(getAllProducts)
 router.route('/static').get(getAllProductsStatic)
